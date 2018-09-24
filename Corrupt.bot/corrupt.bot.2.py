6393f42b158f6a64a6a0e7770128da8b08e32444
Andrew Taylor <43536569+andrewtaylor2004@users.noreply.github.com>
2018-09-24 13:38:00 +0100

    Uploaded the BYTE-ADDER

---
+++ Corrupt.bot/corrupt.bot.2.py
@@ -0,0 +1,52 @@
+###The C++ and C# code are being developed by Phil
+### On a computer that is not in School
+### and that has a "LEGAL" copy of Microsoft
+### Visual Studio.
+###
+### Also the JavaScript scripts may/may not be replaced with
+### some Ruby Scripts.
+###
+### Or maybe I will keep JS and add Ruby aswell
+### Batch and Visual Basic Script coding is being added
+
+cpp= ("1")
+csharp= ("1")
+js=("1")
+byteadderbytes=("1KB")
+
+
+if js== ("0"):
+    print("Please set the variable 'js' to '1'to continue")
+    input()
+else:
+    print("JavaScript has been enabled, checking for C++ and C#")
+    if cpp== ("0"):
+        print("JavaScript is enabled, but C++ isnt!")
+        print("ABORTING CODE at 0x00092")
+        input()
+    else:
+        print("JavaScript and C++ are enabled. Detecting C#")
+    if csharp== ("0"):
+        print("JavaScript and C++ are enabled, but C# isnt!")
+        print("ABORTING CODE at 0x08212")
+        input()
+    else:
+        print("Everything is enabled! Good Job Phil!")
+        print("\n")
+        print("Executing the Code!")
+        print()
+        print("Adding Extra Bytes to memory")
+        print()
+        print()
+        print("Succesfully added",byteadderbytes,"of extra bytes to file:")
+        print("CORRUPT.BOT.BYTEADDED.PY")
+        print()
+        input()
+        else:
+            if addedingdrive==("0"):
+                print("Phil.... you should add this into the Google Drive")
+                input()
+            else:
+                print("You have served a good purpose in life")
+                print("my student")
+                input()
